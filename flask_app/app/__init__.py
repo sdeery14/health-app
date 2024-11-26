c9a9c6bec4fc98662671058f41073e175994b2fe
Sean Deery <sdeery14@gmail.com>
2024-11-26 09:26:46 -0500

    fix docker set up for flask

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('FLASK_DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
